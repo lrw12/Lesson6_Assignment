1b4c5ada26a42d6bb414820466753a461df067ba
lindseywg <lrw12@my.fsu.edu>
2015-10-19 16:53:57 +0000

    Lesson 6 Assignment FINAL COMMIT

--- Lesson6_Assignment/photos.js
+++ Lesson6_Assignment/photos.js
@@ -12,17 +12,24 @@ var photoOrder = [1,2,3,4,5];
 //var autoAdvance = setInterval(rightAdvance,5000);
 var figureCount = 3;
 
-function populateFigures() {
-    var filename;
-    var currentFig;
-
+/* add src values to img elements based on order specified in photoOrder array */
+function populateFigures() {  
+  var filename;
+  var currentFig;
+  if (figureCount === 3) {
         for (var i = 1; i < 4; i++) {
             filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
             currentFig = document.getElementsByTagName("img")[i - 1];
             currentFig.src = filename;
-        }//end of loop
-
-}//end of populateFigures()
+        }
+  } else {
+     for (var i = 0; i < 5; i++) {
+       filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+       currentFig = document.getElementsByTagName("img")[i];
+       currentFig.src = filename;
+    }
+  }
+}
 
 /* shift all images one figure to the right, and change values in photoOrder array to match  */
 function rightArrow() {
@@ -50,18 +57,50 @@ function leftArrow() {
    }
 }
 
-// create event listeners for left arrow, right arrow, and center figure element 
-function createEventListeners() {
-  var leftarrow = document.geteElementById("leftarrow");
+function previewFive() {
   
+  var articleE1 = document.getElementsByTagName("article")[0];
+  
+   // create figure and img elements for fifth image
+   var lastFigure = document.createElement("figure");
+   lastFigure.id = "fig5";
+   lastFigure.style.zIndex = "5";
+   lastFigure.style.position = "absolute";
+   lastFigure.style.right = "45px";
+   lastFigure.style.top = "67px";
+    
+   //articleE1.insertBefore(lastFigure, document.getElementById("rightArrow"));
+   var lastImage = document.createElement("img");
+   lastImage.width = "240";
+   lastImage.height = "135";
+
+   lastFigure.appendChild(lastImage);
+   articleE1.appendChild(lastFigure);
+  
+   //clone figure element for fifth image and edit to be first image
+   var firstFigure = lastFigure.cloneNode(true);
+   firstFigure.id = "fig1";
+   firstFigure.style.right = "";
+   firstFigure.style.left = "45px";
+   articleE1.insertBefore(firstFigure, document.getElementById("fig2"));
+  
+   // add appropriate src values to two new img elements
+   document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+   document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
+  
+   figureCount = 5;
+}//end of previewFive()
+
+/* create event listeners for left arrow, right arrow, and center figure element */
+function createEventListeners() {
+  var leftarrow = document.getElementById("leftarrow");
   if (leftarrow.addEventListener) {
     leftarrow.addEventListener("click", leftArrow, false);
   } else if (leftarrow.attachEvent) {
     leftarrow.attachEvent("onclick", leftArrow);
   }
     
-   var rightarrow - document.getElementById("rightarrow");
-    
+   var rightarrow = document.getElementById("rightarrow");
    if (rightarrow.addEventListener) {
      rightarrow.addEventListener("click", rightArrow, false);
   } else if (rightarrow.attachEvent) {
@@ -69,14 +108,20 @@ function createEventListeners() {
   } 
    
   var mainFig = document.getElementsByTagName("img")[1];
-  
   if (mainFig.addEventListener) {
-        mainFig.addEventListener("click", zoomFig, false); 
-    } else if (mainFig.attachEvent)  {
+        mainFig.addEventListener("click", zoomFig, false);
+  } else if (mainFig.attachEvent) {
         mainFig.attachEvent("onclick", zoomFig);
-    }
+  }
   
-}//end of createEventListeners function  
+  var showAllButton = document.querySelector("#fiveButton p");
+  if (showAllButton.addEventListener) {
+      showAllButton.addEventListener("click", previewFive, false);
+  } else if (showAllButton.attachEvent) {
+      showAllButton.attachEvent("onclick", previewFive);
+  }
+
+}  
 
 
 /* open center figure in separate window */
